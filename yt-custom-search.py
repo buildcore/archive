8a11e3051bba138be518295dc0cff9fc0f1b09ad
core <95249675+buildcore@users.noreply.github.com>
2022-05-23 12:38:04 +0200

    Create yt-custom-search.py

---
+++ yt-custom-search.py
@@ -0,0 +1,27 @@
+
+import pprint
+
+from googleapiclient.discovery import build
+
+
+def main():
+    # Build a service object for interacting with the API. Visit
+    # the Google APIs Console <http://code.google.com/apis/console>
+    # to get an API key for your own application.
+    service = build(
+        "customsearch", "v1", developerKey="<YOUR DEVELOPER KEY>"
+    )
+
+    res = (
+        service.cse()
+        .list(
+            q="lectures",
+            cx="017576662512468239146:omuauf_lfve",
+        )
+        .execute()
+    )
+    pprint.pprint(res)
+
+
+if __name__ == "__main__":
+    main()
